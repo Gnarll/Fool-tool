7dbc5c143e7d62cf3f4a79b81e7b15cbda339620
Gnarll <vadim.fedorovich@ggp.local>
2025-08-05 16:04:16 +0300

    add room compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
+    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
 
