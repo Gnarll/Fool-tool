9f3169f2264e1a0083b13965377ae205a6b64448
Gnarll <vadim.fedorovich22@mail.ru>
2025-08-07 10:50:09 +0300

    test FlashcardRepositoryImpl, refactor test files structure

---
+++ app/src/androidTest/java/com/example/fool_tool/dbTest/FlashcardRepositoryImplTest.kt
@@ -0,0 +1,89 @@
+package com.example.fool_tool.dbTest
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import app.cash.turbine.test
+import com.example.fool_tool.data.local.dao.FlashcardDao
+import com.example.fool_tool.data.local.db.AppDatabase
+import com.example.fool_tool.data.repositories.FlashcardRepository
+import com.example.fool_tool.data.repositories.FlashcardRepositoryImpl
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+class FlashcardRepositoryImplTest {
+    lateinit var db: AppDatabase
+    lateinit var flashcardDao: FlashcardDao
+    lateinit var flashcardRepository: FlashcardRepository
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        flashcardDao = db.flashcardDao()
+        flashcardRepository = FlashcardRepositoryImpl(flashcardDao = flashcardDao)
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun flashcardRepository_insertAndGetFlashcardById_flashcardIsValid() = runTest {
+        val flashcard = DbTestUtils.createFlashcard()
+        flashcardRepository.insertFlashcard(flashcard)
+
+        val fetchedFlashcard = flashcardRepository.getFlashcardById(flashcard.id)
+        assertEquals(flashcard, fetchedFlashcard)
+    }
+
+    @Test
+    fun flashcardRepository_insertAndGetAllFlashcards_flashcardsAreValid() = runTest {
+        val flashcardOne = DbTestUtils.createFlashcard()
+        val flashcardTwo = DbTestUtils.createFlashcard()
+
+        flashcardRepository.insertFlashcard(flashcardOne)
+        flashcardRepository.insertFlashcard(flashcardTwo)
+
+        flashcardRepository.getAllFlashcardsStream().test {
+            val flashcards = awaitItem()
+            assertEquals(flashcards.size, 2)
+            assertTrue(flashcardOne in flashcards)
+            assertTrue(flashcardTwo in flashcards)
+        }
+    }
+
+    @Test
+    fun flashcardRepository_insertAndDeleteFlashcard_flashcardIsDeleted() = runTest {
+        val flashcard = DbTestUtils.createFlashcard()
+
+        flashcardRepository.insertFlashcard(flashcard)
+        val fetchedFlashcard = flashcardRepository.getFlashcardById(flashcard.id)
+        assertEquals(fetchedFlashcard, flashcard)
+
+        flashcardRepository.deleteFlashcard(fetchedFlashcard)
+        flashcardRepository.getAllFlashcardsStream().test {
+            val fetchedFlashcardsList = awaitItem()
+            assertEquals(fetchedFlashcardsList.size, 0)
+        }
+
+    }
+
+    @Test
+    fun flashcardRepository_insertAndUpdateFlashcard_flashcardIsUpdated() = runTest {
+        var flashcard = DbTestUtils.createFlashcard()
+
+        flashcardRepository.insertFlashcard(flashcard)
+        flashcard = flashcard.copy(foreignWord = "Updated")
+        flashcardRepository.updateFlashcard(flashcard)
+
+        val fetchedFlashcard = flashcardRepository.getFlashcardById(flashcard.id)
+        assertEquals(fetchedFlashcard, flashcard)
+    }
+}
---
+++ app/src/androidTest/java/com/example/fool_tool/dbTest/utils.kt
@@ -0,0 +1,21 @@
+package com.example.fool_tool.dbTest
+
+import com.example.fool_tool.data.local.entities.FlashcardEntity
+import com.example.fool_tool.ui.model.Flashcard
+import java.util.UUID
+
+object DbTestUtils {
+    fun createFlashcardEntity(
+        nativeWord: String = "Test",
+        foreignWord: String = "Test",
+        id: Long = UUID.randomUUID().mostSignificantBits
+    ): FlashcardEntity =
+        FlashcardEntity(uid = id, nativeWord = nativeWord, foreignWord = foreignWord)
+
+    fun createFlashcard(
+        nativeWord: String = "Test",
+        foreignWord: String = "Test",
+        id: Long = UUID.randomUUID().mostSignificantBits
+    ): Flashcard =
+        Flashcard(id = id, nativeWord = nativeWord, foreignWord = foreignWord)
+}
