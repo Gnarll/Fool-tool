fa3857241b1bb7b8bcb659cef133a3df9a126ada
Gnarll <vadimfedorovich22@mail.ru>
2025-08-29 16:56:47 +0300

    begin indicator blot animation

--- app/src/main/java/com/example/fool_tool/ui/navigation/utils.kt
+++ app/src/main/java/com/example/fool_tool/ui/navigation/utils.kt
@@ -1,17 +1,46 @@
 package com.example.fool_tool.ui.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavController
+import androidx.compose.ui.draw.CacheDrawScope
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Path
+import kotlin.math.PI
+import kotlin.math.absoluteValue
+import kotlin.math.cos
+import kotlin.math.sin
 
-@Composable
-inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
-    val navGraphRoute = destination.parent?.route ?: return viewModel()
-    val parentEntry = remember(this) {
-        navController.getBackStackEntry(navGraphRoute)
-    }
-    return viewModel(parentEntry)
+
+fun Path.polarLineTo(degrees: Float, distance: Float, origin: Offset = Offset.Zero) {
+    val (x, y) = polarToCart(degrees, distance, origin)
+    lineTo(x, y)
+}
+
+fun Path.polarMoveTo(degrees: Float, distance: Float, origin: Offset = Offset.Zero) {
+    val (x, y) = polarToCart(degrees, distance, origin)
+    moveTo(x, y)
+}
+
+
+fun polarToCart(
+    degrees: Float,
+    distance: Float,
+    origin: Offset = Offset.Zero
+): Offset = Offset(
+    x = distance * cos(degrees * (PI / 180)).toFloat(),
+    y = distance * -(sin(degrees * (PI / 180)).toFloat()),
+) + origin
+
+
+fun CacheDrawScope.calculateDistanceToRectBorder(angleDegrees: Float): Float {
+    val angleRad = Math.toRadians(angleDegrees.toDouble()).toFloat()
+
+    val halfWidth = size.width / 2
+    val halfHeight = size.height / 2
+
+    val dx = cos(angleRad).absoluteValue
+    val lengthToRight = if (dx == 0f) Float.POSITIVE_INFINITY else halfWidth / dx
+
+    val dy = sin(angleRad).absoluteValue
+    val lengthToTop = if (dy == 0f) Float.POSITIVE_INFINITY else halfHeight / dy
+
+    return minOf(lengthToRight, lengthToTop)
 }
