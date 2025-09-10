0c0728a7597867105ddddaffe8f08774eb0ed264
Gnarll <vadimfedorovich22@mail.ru>
2025-09-10 16:47:10 +0300

    implement in-app language changing

--- app/src/main/java/com/example/fool_tool/MainActivity.kt
+++ app/src/main/java/com/example/fool_tool/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.fool_tool
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
@@ -15,7 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var dataStoreManager: DataStoreManager
--- app/src/main/java/com/example/fool_tool/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/fool_tool/ui/screens/settings/SettingsScreen.kt
@@ -1,21 +1,61 @@
 package com.example.fool_tool.ui.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.fool_tool.R
+import com.example.fool_tool.ui.components.settings.LanguagesDropdown
 import com.example.fool_tool.ui.components.settings.ThemeRadioGroup
 
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier, viewModel: SettingsViewModel = hiltViewModel()) {
-    val theme = viewModel.theme.collectAsStateWithLifecycle()
-    
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        ThemeRadioGroup(
-            selectedTheme = theme.value,
-            onSelectTheme = { theme -> viewModel.setTheme(theme) })
+    val theme = viewModel.theme.collectAsStateWithLifecycle().value
+    val language = viewModel.language.collectAsStateWithLifecycle().value
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_x_x_large)),
+            modifier = Modifier
+                .width(IntrinsicSize.Max)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = dimensionResource(R.dimen.surface_shadow_elevation),
+                tonalElevation = dimensionResource(R.dimen.surface_tonal_elevation),
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            ) {
+                ThemeRadioGroup(
+                    selectedTheme = theme,
+                    onSelectTheme = viewModel::setTheme,
+                    Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+            LanguagesDropdown(
+                language = language,
+                onSetLanguage = viewModel::setLanguage,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+            )
+        }
     }
 }
