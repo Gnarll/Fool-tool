a553ebff2231afb3a2641d53e38ff6b916d82d75
Gnarll <vadimfedorovich22@mail.ru>
2025-10-02 09:44:22 +0300

    fix pickers' visual effects

--- app/src/main/java/com/example/fool_tool/ui/components/shared/DatePickerDocked.kt
+++ app/src/main/java/com/example/fool_tool/ui/components/shared/DatePickerDocked.kt
@@ -1,34 +1,29 @@
 package com.example.fool_tool.ui.components.shared
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.window.Popup
 import com.example.fool_tool.R
 import com.example.fool_tool.utils.toLocalDateWithZone
 import com.example.fool_tool.utils.toUtcLocalDate
@@ -52,15 +47,8 @@ fun DatePickerDocked(onDatePicked: (LocalDate) -> Unit) {
         convertMillisToDateString(it)
     } ?: ""
 
-    LaunchedEffect(datePickerState) {
-        val selectedUtcMillis = datePickerState.selectedDateMillis
-        if (selectedUtcMillis != null) {
-            onDatePicked(
-                selectedUtcMillis.toLocalDateWithZone()
-            )
 
-        }
-    }
+    val dismiss = { showDatePicker = false }
 
     Box(
         modifier = Modifier.fillMaxWidth()
@@ -84,21 +72,30 @@ fun DatePickerDocked(onDatePicked: (LocalDate) -> Unit) {
         )
 
         if (showDatePicker) {
-            Popup(
-                onDismissRequest = { showDatePicker = false },
-                alignment = Alignment.TopStart
-            ) {
-                Box(
+            PickerDialog(
+                onConfirm = {
+                    val selectedUtcMillis = datePickerState.selectedDateMillis
+                    if (selectedUtcMillis != null) {
+                        onDatePicked(
+                            selectedUtcMillis.toLocalDateWithZone()
+                        )
+
+                    }
+
+                    dismiss()
+                },
+                onDismiss = {
+                    dismiss()
+                }) {
+
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .offset(y = dimensionResource(R.dimen.text_field_default_height))
-                        .shadow(elevation = dimensionResource(R.dimen.elevation_default))
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(dimensionResource(R.dimen.padding_large))
+                        .verticalScroll(rememberScrollState())
                 ) {
                     DatePicker(
                         state = datePickerState,
                         showModeToggle = false
+
                     )
                 }
             }
