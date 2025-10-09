971ce593e994451f2400f27f24c6f162f7960966
Gnarll <vadimfedorovich22@mail.ru>
2025-10-09 08:03:33 +0300

    implement room paging for reminders

--- app/src/main/java/com/example/fool_tool/data/repositories/ReminderRepository.kt
+++ app/src/main/java/com/example/fool_tool/data/repositories/ReminderRepository.kt
@@ -1,5 +1,9 @@
 package com.example.fool_tool.data.repositories
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.fool_tool.data.local.dao.ReminderDao
 import com.example.fool_tool.data.local.entities.toReminder
 import com.example.fool_tool.ui.model.Reminder
@@ -9,20 +13,33 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface ReminderRepository {
-    fun getAllRemindersStream(): Flow<List<Reminder>>
+    fun getPagedReminders(): Flow<PagingData<Reminder>>
     suspend fun createReminder(reminder: Reminder)
     suspend fun updateReminder(reminder: Reminder)
     suspend fun deleteReminder(reminder: Reminder)
 }
 
-class ReminderRepositoryImpl @Inject constructor(private val reminderDao: ReminderDao) :
+class ReminderRepositoryImpl @Inject constructor(
+    private val reminderDao: ReminderDao,
+) :
     ReminderRepository {
-    override fun getAllRemindersStream(): Flow<List<Reminder>> =
-        reminderDao.getAll().map { reminderEntities ->
-            reminderEntities.map {
-                it.toReminder()
+    override fun getPagedReminders(): Flow<PagingData<Reminder>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                initialLoadSize = 40,
+                prefetchDistance = 10,
+                enablePlaceholders = false
+            )
+        ) {
+            reminderDao.getPagingSource()
+        }.flow
+            .map { pagingData ->
+                pagingData.map { reminderEntity ->
+                    reminderEntity.toReminder()
+                }
             }
-        }
+    }
 
     override suspend fun createReminder(reminder: Reminder) {
         reminderDao.insert(reminder.toReminderEntity())
