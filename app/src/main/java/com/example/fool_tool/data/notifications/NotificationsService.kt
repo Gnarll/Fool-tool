b4e8e2e77f6257e79c3748cebb4978943f22bd6b
Gnarll <vadimfedorovich22@mail.ru>
2026-01-29 16:17:34 +0300

    implement sending a notification

--- app/src/main/java/com/example/fool_tool/data/notifications/NotificationsService.kt
+++ app/src/main/java/com/example/fool_tool/data/notifications/NotificationsService.kt
@@ -1,6 +1,10 @@
 package com.example.fool_tool.data.notifications
 
+import com.example.fool_tool.ui.model.Reminder
+
 interface NotificationsService {
+
+    fun sendReminderNotification(reminder: Reminder)
     fun checkPrimaryPermissionIsGranted(): Boolean
     fun checkReminderChannelPermissionIsGranted(): Boolean
     fun openReminderChannelPermissionSettings(channelId: String)
--- app/src/main/java/com/example/fool_tool/data/notifications/NotificationsServiceImpl.kt
+++ app/src/main/java/com/example/fool_tool/data/notifications/NotificationsServiceImpl.kt
@@ -2,13 +2,19 @@ package com.example.fool_tool.data.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
+import androidx.core.app.NotificationCompat
+import com.example.fool_tool.MainActivity
 import com.example.fool_tool.R
+import com.example.fool_tool.ui.model.Reminder
+import com.example.fool_tool.utils.toShortNotificationContentText
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -22,15 +28,46 @@ class NotificationsServiceImpl @Inject constructor(@ApplicationContext private v
         recreateReminderNotificationsChannel()
     }
 
+    override fun sendReminderNotification(reminder: Reminder) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            addFlags(FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        val pendingIntent =
+            PendingIntent.getActivity(
+                context,
+                reminder.id.toInt(),
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+        val notification = NotificationCompat.Builder(context, REMINDER_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_calendar)
+            .setContentTitle(reminder.title)
+            .setContentText(reminder.description.toShortNotificationContentText())
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(reminder.description)
+            )
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+
+        notificationManager.notify(reminder.id.toInt(), notification)
+    }
+
     override fun checkPrimaryPermissionIsGranted(): Boolean =
         notificationManager.areNotificationsEnabled()
 
     override fun checkReminderChannelPermissionIsGranted(): Boolean {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return true
 
-        val reminderChannel = notificationManager.getNotificationChannel(ChannelsIds.REMINDER)
+        val reminderChannel = notificationManager.getNotificationChannel(REMINDER_CHANNEL_ID)
         return reminderChannel.importance != NotificationManager.IMPORTANCE_NONE
     }
+
 
     override fun openPrimaryPermissionSettings() {
         val intent = Intent().apply {
@@ -63,26 +100,26 @@ class NotificationsServiceImpl @Inject constructor(@ApplicationContext private v
 
     private fun recreateReminderNotificationsChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.getNotificationChannel(ChannelsIds.REMINDER)?.let {
+            notificationManager.getNotificationChannel(REMINDER_CHANNEL_ID)?.let {
                 return
             }
 
             val name = context.getString(R.string.reminders_channel_name)
             val descriptionText = context.getString(R.string.reminders_channel_description)
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channelId = ChannelsIds.REMINDER
+            val importance = NotificationManager.IMPORTANCE_MAX
+            val channelId = REMINDER_CHANNEL_ID
 
-            val mChannel = NotificationChannel(channelId, name, importance)
-            mChannel.description = descriptionText
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+                setShowBadge(false)
+            }
 
-            notificationManager.createNotificationChannel(mChannel)
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
-
-    object ChannelsIds {
-        const val REMINDER = "reminder-notification-channel-id"
-
+    companion object {
+        const val REMINDER_CHANNEL_ID = "reminder-notification-channel-id"
     }
 
 }
