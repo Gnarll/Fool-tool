7456b535970a85ab825fb3e1b083b1576926cc60
Gnarll <vadimfedorovich22@mail.ru>
2026-02-03 09:13:04 +0300

    fix reminders' date managing

--- app/src/main/java/com/example/fool_tool/data/local/Converters.kt
+++ app/src/main/java/com/example/fool_tool/data/local/Converters.kt
@@ -3,20 +3,22 @@ package com.example.fool_tool.data.local
 import androidx.room.TypeConverter
 import java.time.Instant
 import java.time.LocalDateTime
-import java.time.ZoneId
 import java.time.ZoneOffset
 
 class Converters {
     @TypeConverter
     fun fromLocalDateTime(localDateTime: LocalDateTime?): Long? {
-        return localDateTime?.toInstant(ZoneOffset.UTC)?.toEpochMilli()
+        return localDateTime?.atZone(ZoneOffset.UTC)?.toInstant()?.toEpochMilli()
     }
 
     @TypeConverter
     fun toLocalDateTime(timestamp: Long?): LocalDateTime? {
         return timestamp?.let {
-            LocalDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.systemDefault())
+            Instant.ofEpochMilli(it)
+                .atZone(ZoneOffset.UTC)
+                .toLocalDateTime()
         }
     }
+
 
 }
